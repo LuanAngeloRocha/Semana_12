41fc40c3fde2a2eaa31fafc5d3bb414e4f3d9b1e
LuanAngeloRocha <Luanrochaangelo@gmail.com>
2021-10-21 19:55:20 +0000

    b

---
+++ Empregado.java
@@ -0,0 +1,7 @@
+public class Empregado extends Pessoa {
+    private int codigoSetor;
+    private double salarioBase;
+    private double imposto;
+
+    // fazer os metodos gets e sets
+}
---
+++ Pessoa.java
@@ -0,0 +1,60 @@
+public class Pessoa {
+    private String nome;
+    private String telefone;
+    private String endereco;
+
+
+
+    // Fazer os metodos de acesso publico 
+    //Get e Set
+    public void setNome(String nome){
+        this.nome=nome;
+
+        
+    }
+    //get
+    public String getNome(){
+        return this.nome;
+        
+    }
+    public void setTelefone(String telefone){
+        this.telefone=telefone;
+
+        
+    }
+    //get
+    public String getTelefone(){
+        return this.telefone;
+    }
+    public void setEndereco(String endereco){
+        this.endereco=endereco;
+
+        
+    }
+    //get
+    public String setEndereco(){
+        return this.endereco;
+    }
+    //Contrutor padrão, tem o nome da classe e sem instancia 
+    
+    //parametros
+    public Pessoa(){
+
+    }
+
+//Construtor com um parametro 
+    public Pessoa(String nome ){
+        this.nome=nome;
+
+    }
+//Constrututor com dois parametros 
+
+    public Pessoa(String nome, String telefone){
+        this.nome=nome;
+        this.telefone=telefone;
+
+    }
+
+
+    
+}
